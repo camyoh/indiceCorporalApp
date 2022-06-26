ed94a71d5a648537750739eaff6e4905154a3dca
Andres <carlos.camyoh@gmail.com>
2022-06-26 18:04:16 -0500

    creando IBoutlet de los sliders y calculando el imc con los valores de los sliders

--- IndiceMasaCorporaApp/ViewController.swift
+++ IndiceMasaCorporaApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var labelAltura: UILabel!
     @IBOutlet weak var labelPeso: UILabel!
+    @IBOutlet weak var sliderAltura: UISlider!
+    @IBOutlet weak var sliderPeso: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +26,11 @@ class ViewController: UIViewController {
         labelPeso.text = "\(Int(sender.value)) Kg"
     }
     
+    @IBAction func calcularPresionado(_ sender: UIButton) {
+        let altura = sliderAltura.value
+        let peso = sliderPeso.value
+        let imc = peso/pow(altura, 2)
+        print(imc)
+    }
 }
 
