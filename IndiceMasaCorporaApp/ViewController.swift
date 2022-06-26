bc73f8fa63b6b6229c27f4d6df3221eca21e2026
Andres <carlos.camyoh@gmail.com>
2022-06-26 14:30:58 -0500

    Initial Commit

---
+++ IndiceMasaCorporaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IndiceMasaCorporaApp
+//
+//  Created by Andres Mendieta on 6/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
