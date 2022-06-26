1e3357b1902b11b94abe9102ca2d31ad435b8d61
Andres <carlos.camyoh@gmail.com>
2022-06-26 18:22:33 -0500

    creando carpetas MVC y creando nuevo viewcontroller con cocoatouch new file

---
+++ IndiceMasaCorporaApp/Controllers/ResultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultViewController.swift
+//  IndiceMasaCorporaApp
+//
+//  Created by Andres Mendieta on 6/26/22.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
