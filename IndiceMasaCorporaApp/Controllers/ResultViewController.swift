8d740f12089801eac11ae81726672bef6ba12b6e
Andres <carlos.camyoh@gmail.com>
2022-06-26 18:25:44 -0500

    asignando viewController en storyboard, crenado IBoutlets y IBAction

--- IndiceMasaCorporaApp/Controllers/ResultViewController.swift
+++ IndiceMasaCorporaApp/Controllers/ResultViewController.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    @IBOutlet weak var labelIMC: UILabel!
+    @IBOutlet weak var labelConsejo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    @IBAction func recalcularPresionado(_ sender: UIButton) {
     }
 }
